dd68a48cc360f8a60ff589675ae96968e5a5afb1
antonioajsl1975 <antonioajsl1975@gmail.com>
2025-01-22 00:13:01 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
+@Table(name = "tb_game")
 public class Game {
 
     @Id
@@ -15,18 +16,24 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {}
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -68,6 +75,12 @@ public class Game {
         return platforms;
     }
 
+    public Double getScore() {
+        return score;
+    }
+    public void setScore(Double score) {
+        this.score = score;
+    }
     public void setPlatforms(String platforms) {
         this.platforms = platforms;
     }
